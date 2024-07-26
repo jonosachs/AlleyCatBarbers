ef8e995a0c982c7b47488d93d07324e1a2e17934
Jono <Jono@WINDOWS-TE9FUR6>
2024-07-26 17:38:21 +1000

    Email send button functionality

--- AlleyCatBarbers/wwwroot/js/emailFormButton.js
+++ AlleyCatBarbers/wwwroot/js/emailFormButton.js
@@ -1,4 +1,12 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var form = document.querySelector('form[data-email-sent]');
+    var emailSent = form.getAttribute('data-email-sent');
+
+    if (emailSent) {
+        markButtonAsSent();
+    }
+});
+
 function disableSubmitButton() {
     var button = document.getElementById("submitButton");
     button.disabled = true;
@@ -9,4 +17,11 @@ function enableSubmitButton() {
     var button = document.getElementById("submitButton");
     button.disabled = false;
     button.value = "Send";
+}
+
+function markButtonAsSent() {
+    var button = document.getElementById("submitButton");
+    button.disabled = true;
+    button.value = "Send";
+    button.classList.add('disabled');
 }
